f3197914a6ea838425202a98b554aeb1969006d5
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2021-11-11 21:19:01 +0300

    Write test for App and logo

---
+++ src/tests/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from '../../components/App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('renders Timescale <img> logo', () => {
+  const { getByAltText, container } = render(<App />);
+  expect(container.querySelector(`img[alt="timescale_logo"]`)).toBeDefined(); // checks that logo is an <img> tag
+  expect(getByAltText('timescale_logo')).toBeDefined(); // checks its alt text
+});
